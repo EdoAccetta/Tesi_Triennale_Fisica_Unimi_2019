b2f596a0ec9d6b7e4b5c69d853b20e2012d9c282
alex <alexbonazzi5652@gmail.com>
2019-07-29 13:37:09 +0200

    - Messa prima bozza con libreria diversa di iphone_csv_to_json_to_graph.py - Sistemato acc_fje_to_json.py in modo tale che si parsi tutti i files e crei i JSON - Formattati files example.json sia in iphone che acc - Aggiunti file accellerometro trasportati in JSON - Aggiunto .gitignore per VSCode - Aggiunto file requirements.txt

--- src/acc_fje_to_json.py
+++ src/acc_fje_to_json.py
@@ -1,34 +1,42 @@
 import json
 from datetime import datetime, timedelta
+from os import listdir
+from os.path import isfile, join
 
-# Apriamo il file
-tempi = []
-i = 0
+path_acc_data = "../data/acc"
+path_acc_data_fje = path_acc_data + "/fje/"
+path_acc_data_json = path_acc_data + "/json/"
+list_files = [f for f in listdir(path_acc_data_fje) if isfile(join(path_acc_data_fje, f))]
 
-path_acc_data = "../data/acc/"
-with open(path_acc_data + "fje/example.fje","r") as file_acc:
-    # Leggiamo
-    for line in file_acc:
-        valori = line.split(",")
+for file in  list_files:
 
-        linea = {}
+    # Apriamo il file
+    tempi = []
+    i = 0
 
-        #Acc X	Acc Y	Acc Z	V ang X	V ang Y	V ang Z	Latitudine	Longitudine	Timestamp
+    with open(path_acc_data_fje + file,"r") as file_acc:
+        # Leggiamo
+        for line in file_acc:
+            valori = line.split(",")
 
-        linea["step"] = i
-        
-        linea["gFx"] = valori[0]
-        linea["gFy"] = valori[1]
-        linea["gFz"] = valori[2]
-        linea["Vax"] = valori[3]
-        linea["Vay"] = valori[4]
-        linea["Vaz"] = valori[5]
-        linea["time"] = valori[8].replace("\n", "")
+            linea = {}
 
-        tempi.append(linea)
-        i = i+1
+            #Acc X	Acc Y	Acc Z	V ang X	V ang Y	V ang Z	Latitudine	Longitudine	Timestamp
+
+            linea["step"] = i
+            
+            linea["gFx"] = valori[0]
+            linea["gFy"] = valori[1]
+            linea["gFz"] = valori[2]
+            linea["Vax"] = valori[3]
+            linea["Vay"] = valori[4]
+            linea["Vaz"] = valori[5]
+            linea["time"] = valori[8].replace("\n", "")
+
+            tempi.append(linea)
+            i = i+1
 
 
 
-with open(path_acc_data + "json/example.json","w") as file_acc:
-    json.dump(tempi, file_acc)
+    with open(path_acc_data_json + file.replace(".fje", ".json"),"w") as file_acc:
+        json.dump(tempi, file_acc)
--- src/iphone_csv_to_json_to_graph.py
+++ src/iphone_csv_to_json_to_graph.py
@@ -1,61 +1,22 @@
 import json
-import matplotlib.pyplot as plt #library used for simple plots 
-from datetime import datetime, timedelta
+import plotly.graph_objects as go
+import numpy as np
 
-# Apriamo il file
-tempi = []
-i = 0
-first_time = datetime.strptime("00:00.0", "%M:%S.%f")
-one_hour = datetime.strptime("01", "%M")
-gravita = 9.80665
+path_iphone_data = "C:/Users/mtollari/Desktop/Code/tesi_dido/data/iphone/json/example.json"
+with open(path_iphone_data,"r") as file_iphone:
+    tempi = json.load(file_iphone)
 
-path_iphone_data = "../data/iphone/"    #This is for the old example file
-with open(path_iphone_data + "csv/example.csv","r") as file_iphone:
+    steps = np.array([])
+    gfys = np.array([])
 
+    for tempo in tempi:
+        steps = np.append(steps, int(tempo["step"]))
+        gfys = np.append(gfys, float(tempo["gFy"]))
 
-#path_iphone_data = "../data/iphone/"    #This is ok for the new files, but they are formatted differently and need a different approach ||
-#with open(path_iphone_data + "csv/0001 - (1) - IPH.csv","r") as file_iphone:   # || ValueError: time data 'time,gFx,gFy,gFz,gFTotal\n' does not match format '%M:%S.%f'
+    plt = go.Figure()
+    plt.add_trace(go.Scatter(x=steps, y=gfys,
+                    mode='lines',
+                    name='gianpiero'))
+    
 
-    # Leggiamo
-    for line in file_iphone:
-        valori = line.split(";")
-
-        linea = {}
-        
-        time = datetime.strptime( valori[0], "%M:%S.%f")
-
-        if i == 0:
-            first_time = time
-
-        diff_time = time - first_time
- 
-        # The given data can start from 59:59.00 and goes to 00:00.00, therefore if we detect a negative delta of the days we add one hour to the current time
-        if diff_time.days < 0:
-            time = datetime.strptime( "01:" + valori[0], "%H:%M:%S.%f")
-            diff_time = time - first_time
-
-        linea["step"] = i
-        linea["time"] = str(diff_time)
-        linea["gFx"] = float(valori[1]) * gravita
-        linea["gFy"] = float(valori[2]) * gravita
-        linea["gFz"] = float(valori[3]) * gravita
-        linea["gFTotal"] = valori[4].replace("\n", "")
-
-
-        tempi.append(linea)
-        i = i+1
-
-#numero = len(tempi)    #in future we'll surely need to know the vector lenght to do stuff
-#for n in range (0, tempi[numero-1]["step"])    #tried to go for a cycle to fill the plot but i don't think it works, it needs the full vector
-#   plt.plot(int(tempi[n]["step"]), float(tempi[n]["gFy"]))
-
-plt.plot(int(tempi["step"]), float(tempi["gFy"]))   #TypeError: list indices must be integers or slices, not str || I don't get why these would still be considered strings
-
-plt.xlabel('Step')  #gives x-axis a label
-plt.ylabel('gFy  value') #gives y-axis a label
-plt.title('Acceleration on Y axes (Vertical)')  #gives a title to the plot
-plt.show()  #actually shows the plot on screen
-
-
-with open(path_iphone_data + "json/example.json","w") as file_iphone:
-    json.dump(tempi, file_iphone)
+    plt.show()
