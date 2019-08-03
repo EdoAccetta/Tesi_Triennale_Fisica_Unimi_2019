edc3279b0dacc5d63ae09cd3b55f1c35c0ad4e77
mtolla <mattia@tollari.io>
2019-08-03 10:28:30 +0200

    Sistemato parse iphone CSV -> JSON Aggiunti files parsati in .json Tolto example ormai non più conforme

--- src/iphone_csv_to_json.py
+++ src/iphone_csv_to_json.py
@@ -1,45 +1,56 @@
 import json
 from datetime import datetime, timedelta
+from os import listdir
+from os.path import isfile, join
 
-# Apriamo il file
-tempi = []
-i = 0
-first_time = datetime.strptime("00:00.0", "%M:%S.%f")
-one_hour = datetime.strptime("01", "%M")
-gravita = 9.80665
+path_iphone_data = "../data/iphone"
+path_iphone_data_csv = path_iphone_data + "/csv/"
+path_iphone_data_json = path_iphone_data + "/json/"
+list_files = [f for f in listdir(path_iphone_data_csv) if isfile(join(path_iphone_data_csv, f))]
 
-path_iphone_data = "../data/iphone/"
-with open(path_iphone_data + "csv/example.csv","r") as file_iphone:
-    # Leggiamo
-    for line in file_iphone:
-        valori = line.split(";")
+for file in  list_files:
+    # Apriamo il file
+    tempi = []
+    i = 0
+    first_time = datetime.strptime("00:00.0", "%M:%S.%f")
+    one_hour = datetime.strptime("01", "%M")
+    gravita = 9.80665
 
-        linea = {}
-        
-        time = datetime.strptime( valori[0], "%M:%S.%f")
+    with open(path_iphone_data_csv + file,"r") as file_iphone:
+        # Leggiamo
+        for line in file_iphone:
+            valori = line.split(",")
 
-        if i == 0:
-            first_time = time
+            if valori[0] == "time" or file_iphone == "example.csv":
+                continue
 
-        diff_time = time - first_time
- 
-        # The given data can start from 59:59.00 and goes to 00:00.00, therefore if we detect a negative delta of the days we add one hour to the current time
-        if diff_time.days < 0:
-            time = datetime.strptime( "01:" + valori[0], "%H:%M:%S.%f")
+            linea = {}
+            
+            time = datetime.strptime(valori[0][11:], "%H:%M:%S.%f")
+            
+            if i == 0:
+                first_time = time
+
             diff_time = time - first_time
+    
+            # The given data can start from 59:59.00 and goes to 00:00.00, therefore if we detect a negative delta of the days we add one hour to the current time
+            # if diff_time.days < 0:
+                # time = datetime.strptime( "01:" + valori[0], "%H:%M:%S.%f")
+                # diff_time = time - first_time
 
-        linea["step"] = i
-        linea["time"] = str(diff_time)
-        linea["gFx"] = float(valori[1]) * gravita
-        linea["gFy"] = float(valori[2]) * gravita
-        linea["gFz"] = float(valori[3]) * gravita
-        linea["gFTotal"] = valori[4].replace("\n", "")
+            linea["step"] = i
+            linea["time"] = str(diff_time)
+            linea["timestamp"] = valori[0]
+            linea["gFx"] = float(valori[1]) * gravita
+            linea["gFy"] = float(valori[2]) * gravita
+            linea["gFz"] = float(valori[3]) * gravita
+            linea["gFTotal"] = valori[4].replace("\n", "")
 
 
-        tempi.append(linea)
-        i = i+1
+            tempi.append(linea)
+            i = i+1
 
 
 
-with open(path_iphone_data + "json/example.json","w") as file_iphone:
-    json.dump(tempi, file_iphone)
+    with open(path_iphone_data_json + file.replace(".csv", ".json"),"w") as file_iphone:
+        json.dump(tempi, file_iphone)
